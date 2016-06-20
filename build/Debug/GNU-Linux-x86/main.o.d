192c18d53e70243f462ba6bb44e6b12dccf3498c
Szymon <szymon.kordiaczynski@gmail.com>
2016-06-20 13:04:05 +0200

    almost

---
+++ build/Debug/GNU-Linux-x86/main.o.d
@@ -0,0 +1,43 @@
+build/Debug/GNU-Linux-x86/main.o: main.cpp headers/opencvHeaders.h \
+ headers/WindowController.h headers/opencvHeaders.h headers/WindowView.h \
+ headers/FieldWithText.h headers/ButtonsList.h headers/ElementDetector.h \
+ headers/SearchingAlgorithmBase.h headers/SASimple.h headers/Calibrator.h \
+ headers/ImageSource.h headers/Painter.h headers/WindowView.h \
+ headers/ImageSource.h headers/Calibrator.h headers/ElementDetector.h \
+ headers/Painter.h headers/SASimple.h
+
+headers/opencvHeaders.h:
+
+headers/WindowController.h:
+
+headers/opencvHeaders.h:
+
+headers/WindowView.h:
+
+headers/FieldWithText.h:
+
+headers/ButtonsList.h:
+
+headers/ElementDetector.h:
+
+headers/SearchingAlgorithmBase.h:
+
+headers/SASimple.h:
+
+headers/Calibrator.h:
+
+headers/ImageSource.h:
+
+headers/Painter.h:
+
+headers/WindowView.h:
+
+headers/ImageSource.h:
+
+headers/Calibrator.h:
+
+headers/ElementDetector.h:
+
+headers/Painter.h:
+
+headers/SASimple.h:
